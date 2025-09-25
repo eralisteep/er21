b84bcecc3921532d10e8fdd63569a23b08017619
eralisteep <eralirahat@gmail.com>
2025-09-25 09:00:09 +0500

    verify update

--- src/app/api/auth/verify/route.js
+++ src/app/api/auth/verify/route.js
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
-import firebaseAdmin from "@/src/utils/firebaseAdmin";
+import admin from "@/src/utils/firebaseAdmin";
+
+const db = admin.firestore(); // Firestore через Admin SDK
 
 export async function POST(req) {
   try {
@@ -14,8 +16,15 @@ export async function POST(req) {
 
     if (!token) return NextResponse.json({ error: "No Token" }, { status: 401 });
 
-    const decodedToken = await firebaseAdmin.auth().verifyIdToken(token);
-    return NextResponse.json({ role: decodedToken.role || "student" });
+    const decodedToken = await admin.auth().verifyIdToken(token);
+    if (!decodedToken.role) {
+      const snapshot = await db.collection("users").get();
+      const users = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+      const userRole = users.find((u) => u.id === decodedToken.uid)?.role;
+      return NextResponse.json({ role: userRole}); // По умолчанию "student"
+    } else {
+      return NextResponse.json({ role: decodedToken.role});
+    }
   } catch (error) {
     console.error("Ошибка верификации:", error);
     return NextResponse.json({ error: "Invalid Token" }, { status: 403 });
